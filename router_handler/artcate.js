38ab6f60d02b7379e882778ad1cd5b96251e6a7f
ganchunmei <1242829205@qq.com>
2020-11-14 18:26:51 +0800

    添加分类信息

--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -21,7 +21,40 @@ exports.getArticleCates = (req, res) => {
 }
 
 //2.新增分类的路由处理函数
-//
+//思路：校验用户输入的数据,判断数据库中是否有和数据库中的分类名称或者分类别名相同，如果相同就提示用户名称重复，如果灭有就加入
 exports.addArticleCates = (req, res) => {
-    res.send("ok");
+    // res.send("ok")
+    //获取用户输入的数据
+    const data = req.body;
+    //创建sql语句，用于判断数据库中是否有相同name或者相同alias的分类
+    const sqlStr = `select * from ev_article_cate where name=? or alias=?`;
+    //执行sql语句
+    db.query(sqlStr, [data.name, data.alias], (err, results) => {
+        if (err)
+            return res.crs(err);
+        if (results.length === 2)
+            return res.crs("分类名称和分类别名都被占用");
+        if (results.length == 1 && results[0].name === data.name && results[0].alias === data.alias)
+            return res.crs("分类名称和分类别名都别占用");
+        if (results.length === 1 && results[0].name === data.name)
+            return res.crs("分类名称已经被占用");
+        if (results.length === 1 && results[0].alias === data.alias)
+            return res.crs("分类别名被占用");
+
+        //如果分类名称和分类别名都不重复，就执行添加操作
+        const sql = `insert into ev_article_cate set ?`;
+        //执行添加分类的语句
+        db.query(sql, data, (err, results) => {
+            if (err)
+                return res.crs(err);
+            //如果没有添加进去
+            if (results.affectedRows !== 1) {
+                return res.crs("数据添加失败");
+            }
+            return res.send({
+                status: 0,
+                message: "分类添加成功"
+            })
+        })
+    })
 }
